079545802bc0fcd3a60a2de0a4780c14c61702bc
Hafiz <muhdhafiizz@gmail.com>
2025-04-16 16:44:04 +0800

    Initial Commit

---
+++ recipe_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  recipe_ios
+//
+//  Created by Muhammad Hafiz Mohd Azahar on 16/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ recipe_iosTests/recipe_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_iosTests.swift
+//  recipe_iosTests
+//
+//  Created by Muhammad Hafiz Mohd Azahar on 16/04/2025.
+//
+
+import Testing
+@testable import recipe_ios
+
+struct recipe_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
